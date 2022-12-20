0c589e09fb1ddb2ace893d35049ed6e60d9c7817
brunosas88 <bruno.sas@live.com>
2022-12-20 18:42:45 -0300

    Adding menu and related methods options
    The menu as it is now offers the
    options to add new players
    and initiate a new game

--- Tic-Tac-Toe/Game.cs
+++ Tic-Tac-Toe/Game.cs
@@ -10,9 +10,160 @@ namespace Tic_Tac_Toe
 	{
 		static void Main(string[] args)
 		{
-			Board gameBoard = new Board();
+			Console.BackgroundColor = ConsoleColor.DarkCyan;
+			Console.ForegroundColor = ConsoleColor.White;
+			List<Player> players = new List<Player>();
+			int option;
+			string warningMessage = "";
+			bool warning = false, validEntry;
+
+			do
+			{
+				Display.GameInterface("Menu Inicial");
+				Display.ShowMenu();
+
+				if (warning)
+				{
+					Display.ShowWarning(warningMessage);
+					warning = false;
+				}
+
+				Console.Write("Escolha operação a ser realizada indicando seu número: ");
+
+				validEntry = int.TryParse(Console.ReadLine(), out option);
+
+				switch (option)
+				{
+					case 1:
+						RegisterPlayer(players);
+						break;
+					case 2:
+						
+						break;
+					case 3:
+
+						break;
+
+					case 0:
+					default:
+						if (validEntry && option == 0)
+						{
+							Display.GameInterface("Muito Obrigado por utilizar nosso aplicativo!");
+							break;
+						}
+						else
+							option = -1;
+						warningMessage = "Aviso: Opção inválida, favor inserir número de 0 a 3.";
+						warning = true;
+						break;
+				}
+			} while (option != 0);
+
+		}
+
+		static Player SelectPlayer(List<Player> players, int playerOrder)
+		{
+			string findPlayerAgain = "n";
+			Player? player;
+
+			do
+			{
+				Display.GameInterface("Configurações Iniciais do Jogo");
+
+				Console.Write($"Nome do Jogador {playerOrder}: ");
+				string dataEntry = Console.ReadLine();
+
+				player = players.Find(player => player.Nome == dataEntry);
+
+				if (player == null)
+				{
+					Display.ShowWarning("Jogador Não Encontrado!");
+					Console.Write("Selecionar Outro Jogador? S - sim / Qualquer outra tecla - não: ");
+					findPlayerAgain = Console.ReadLine();
+				}
+				else
+					return player;
+
+			} while (findPlayerAgain == "s" || findPlayerAgain == "S");
+
+			return player;
 			
-			int player = 1, position, winner;
+		}
+
+		static bool SelectGameOptions(List<Player> players)
+		{
+			Display.GameInterface("Configurações Iniciais do Jogo");
+	
+			Player? player = SelectPlayer(players, 1);
+
+			Player? otherPlayer = SelectPlayer(players, 2);
+
+			if (player == null || otherPlayer == null)			
+				Display.ShowWarning("Jogador(es) Inválidos!");
+			else
+			{
+				player.PlayOrder = 1;
+				otherPlayer.PlayOrder = 2;
+				int winner;
+				string playAgain = "n";
+				do
+				{
+					winner = PlayGame(player, otherPlayer);
+
+					if(winner == 1)					
+						player.Vctories++;					
+					else if (winner == 2)
+						otherPlayer.Vctories++;
+					else
+					{
+						player.Draws++;
+						otherPlayer.Draws++;
+					}
+
+					Console.Write("Selecionar Outro Jogador? S - sim / Qualquer outra tecla - não: ");
+					playAgain = Console.ReadLine();
+					
+
+				
+
+				} while (playAgain == "s" || playAgain == "S");
+			}
+
+			Display.BackToMenu();
+
+			return true;
+		}
+
+		public static void RegisterPlayer(List<Player> players)
+		{
+			string name, cpf, password, warning = "Jogador já Cadastrado!";
+			bool isRegistered;
+			Display.GameInterface("Cadastro de Novo Jogador");	
+
+			Console.Write("Insira nome do novo cliente: ");
+			name = Console.ReadLine();
+
+			isRegistered = players.Exists(player => player.Nome == name);
+
+			if (!string.IsNullOrEmpty(name) && !isRegistered)
+			{
+				players.Add(new Player(name));				
+				warning = $"Jogador {name} Cadastrado com Sucesso!";
+			}
+			else
+				warning += " Operação Não Realizada!";
+
+			Display.ShowWarning(warning);
+
+			Display.BackToMenu();
+		}
+
+		static int PlayGame(Player playerOne, Player playerTwo)
+		{
+			Board gameBoard = new Board();
+			Player currentPlayer = playerOne; 
+
+			int position, winner;
 			bool playerOneTurn = true;
 			bool[] moveCount = new bool[9];
 
@@ -20,13 +171,13 @@ namespace Tic_Tac_Toe
 			{
 				Console.Clear();
 				gameBoard.PrintBoard();
-				Console.Write($"Jogador {player}, insira posição: ");
+				Console.Write($"Jogador {currentPlayer.Nome}, insira posição: ");
 				position = CheckMove(moveCount);
 				if (position != 0)
 				{
-					gameBoard.UpdateBoard(position, player);					
+					gameBoard.UpdateBoard(position, currentPlayer.PlayOrder);
 					playerOneTurn = !playerOneTurn;
-					player = playerOneTurn ? 1 : 2;
+					currentPlayer = playerOneTurn ? playerOne : playerTwo;
 				}
 				winner = CheckWinner(gameBoard.GetBoard());
 				if (!moveCount.Contains(false))
@@ -35,13 +186,15 @@ namespace Tic_Tac_Toe
 
 			if (winner == 0)
 			{
-				Console.WriteLine("Jogadores finalizaram a partida sem resultado final obtido");
+				Console.WriteLine("Jogadores finalizaram a partida sem vencedores");
 			}
 			else
-				Console.WriteLine($"Jogador {winner} venceu");
+				Console.WriteLine($"Jogador {currentPlayer.Nome} venceu");
+
+			return winner;
 		}
 
-		public static int CheckWinner(char[,] board)
+		static int CheckWinner(char[,] board)
 		{
 			char result = ' ';
 
@@ -64,7 +217,7 @@ namespace Tic_Tac_Toe
 			return result == 'X' ? 1 : result == 'O' ? 2 : 0;
 		}
 
-		public static int CheckMove(bool[] moveCount)
+		static int CheckMove(bool[] moveCount)
 		{
 			int position;
 			bool validEndtry;
