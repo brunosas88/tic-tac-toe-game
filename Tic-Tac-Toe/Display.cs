b050a76191156b6b97ad8dab6096163d95f14c1b
brunosas88 <bruno.sas@live.com>
2022-12-22 11:22:49 -0300

    Adding method to center align console readings

--- Tic-Tac-Toe/Display.cs
+++ Tic-Tac-Toe/Display.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,40 @@ namespace Tic_Tac_Toe
 {
 	class Display
 	{
+		public static string FormatConsoleReadLine()
+		{
+			Console.ForegroundColor= ConsoleColor.Yellow;
+			string dataEntry = string.Empty;
+			ConsoleKey dataEntryKey;
+			do
+			{
+				ConsoleKeyInfo dataEntryKeyInfo = Console.ReadKey(intercept: true);
+				dataEntryKey = dataEntryKeyInfo.Key;
+
+				if (dataEntryKey == ConsoleKey.Backspace && dataEntry.Length > 0)
+				{					
+					dataEntry = dataEntry[0..^1];
+					Console.CursorLeft = 0;
+					Console.Write(AlignMessage(dataEntry));
+				}
+				else if (!char.IsControl(dataEntryKeyInfo.KeyChar))
+				{
+					dataEntry += dataEntryKeyInfo.KeyChar;
+					Console.CursorLeft = 0;					
+					Console.Write(AlignMessage(dataEntry));					
+				}
+			} while (dataEntryKey != ConsoleKey.Enter);
+			Console.WriteLine();
+			Console.ForegroundColor = ConsoleColor.White;
+			return dataEntry;
+		}
+
 		public static void ShowWarning(string message)
 		{
 			Console.WriteLine();
 			Console.ForegroundColor = ConsoleColor.Black;
 			Console.BackgroundColor = ConsoleColor.Yellow;
-			Console.Write(message + "\n");
+			Console.Write(AlignMessage(message) + "\n");
 			Console.ForegroundColor = ConsoleColor.White;
 			Console.BackgroundColor = ConsoleColor.DarkCyan;
 			Console.WriteLine();
@@ -22,11 +51,11 @@ namespace Tic_Tac_Toe
 		public static void BackToMenu()
 		{
 			Console.WriteLine();
-			Console.WriteLine("Pressione Enter para voltar ao menu anterior.");
+			Console.WriteLine(AlignMessage("Pressione Enter para voltar ao menu anterior."));
 			Console.ReadLine();
 		}
 
-		public static string AlignMessage(string message, int blankSpace)
+		public static string AlignMessage(string message, int blankSpace = Constants.WindowWidthSize)
 		{
 			return String.Format($"{{0,-{blankSpace}}}", String.Format("{0," + ((blankSpace + message.Length) / 2).ToString() + "}", message));
 		}
@@ -34,8 +63,8 @@ namespace Tic_Tac_Toe
 		public static void GameInterface(string message)
 		{
 			Console.Clear();
-			int blankSpace = 52;
-			int totalCharsHeader = 72;
+			int blankSpace = Constants.WindowWidthSize-20;
+			int totalCharsHeader = Constants.WindowWidthSize;
 			message = AlignMessage(message, blankSpace);
 			string title = AlignMessage("Tic-Tac-Toe", blankSpace);
 			Console.BackgroundColor = ConsoleColor.Green;
@@ -59,19 +88,38 @@ namespace Tic_Tac_Toe
 
 		public static void ShowMenu()
 		{
-			Console.WriteLine("1 - Cadastrar Novo Jogador");
-			Console.WriteLine("2 - Histórico do Jogo");
-			Console.WriteLine("3 - Jogar!");
-			Console.WriteLine("0 - Sair do Jogo");
+			Console.WriteLine(AlignMessage("1 - Cadastrar Novo Jogador"));
+			Console.WriteLine(AlignMessage("2 - Histórico do Jogo"));
+			Console.WriteLine(AlignMessage("3 - Jogar!"));
+			Console.WriteLine(AlignMessage("0 - Sair do Jogo"));
 			Console.WriteLine();
 		}
 
 		public static void ShowLogMenu()
 		{
-			Console.WriteLine("1 - Ver Histórico de Jogadores");
-			Console.WriteLine("2 - Ver Histórico de Partidas");
-			Console.WriteLine("0 - Voltar ao Menu Principal");
+			Console.WriteLine(AlignMessage("1 - Ver Histórico de Jogadores"));
+			Console.WriteLine(AlignMessage("2 - Ver Histórico de Partidas"));
+			Console.WriteLine(AlignMessage("0 - Voltar ao Menu Principal"));
 			Console.WriteLine();
+		}
+
+		public static void ShowPlayerDetails(Player player)
+		{
+			Console.WriteLine();
+			Console.WriteLine(Display.AlignMessage($"Nome: {player.Nome}"));
+			Console.WriteLine(Display.AlignMessage($"Vitórias: {player.Victories}"));
+			Console.WriteLine(Display.AlignMessage($"Derrotas: {player.Defeats}"));
+			Console.WriteLine(Display.AlignMessage($"Empates: {player.Draws}"));
+		}
+
+		public static void ShowMatchesDetails(Match match)
+		{
+			Console.WriteLine();
+			Console.WriteLine(Display.AlignMessage($"{match.PlayerOne} vs {match.PlayerTwo}"));
+			Console.WriteLine(Display.AlignMessage($"{match.PlayerOneVictories} x {match.PlayerTwoVictories}"));
+			Console.WriteLine(Display.AlignMessage($"Empates : {match.Draws}"));
+			if (match.MatchesPlayed > 1)
+				Console.WriteLine(Display.AlignMessage($"Partidas Consecutivas: {match.MatchesPlayed}"));
 		}
 	}
 }
